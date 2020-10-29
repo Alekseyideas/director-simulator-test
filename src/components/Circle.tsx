bd81b8f12d8ab1446d6a42ccff340df1fb73fb13
Aleksey Zelenskiy <alekseyzelenskiy@Alekseys-MacBook-Pro.local>
2020-10-29 17:59:55 +0200

    updates

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -3,10 +3,13 @@ import './Circle.scss';
 
 interface CircleProps {
   num: number;
+  currentQuestion: number;
 }
 
-export const Circle: React.FC<CircleProps> = ({ num }) => {
+export const Circle: React.FC<CircleProps> = ({ num, currentQuestion }) => {
   return (
-    <div className='progress-circle' id='circle' data-progress={num === Infinity ? 0 : num}></div>
+    <div className='progress-circle' id='circle' data-progress={num === Infinity ? 0 : num}>
+      <span>{currentQuestion + 1}</span>
+    </div>
   );
 };
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,15 +4,22 @@ interface ButtonProps {
   onClick: () => void;
   classes?: string;
   title?: string;
+  disabled?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   onClick,
   classes,
+  disabled,
   title = 'Наступне запитання',
 }) => {
   return (
-    <button type='button' className={`tda__btn ${classes || ''}`} onClick={onClick}>
+    <button
+      type='button'
+      className={`tda__btn ${classes || ''}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {title}
     </button>
   );
